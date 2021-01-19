2cc49376b0017c76a58e1e962178f040e7b77012
admin <dharsi.p@gmail.com>
2021-01-19 10:41:53 -0500

    FirstCommit

---
+++ app/src/main/java/com/dharsi/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dharsi.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);    }
+}
